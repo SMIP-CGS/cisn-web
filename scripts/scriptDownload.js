deddd30f7d87e7a345ff85ab34558d641f056d4f
Jennifer Taggart <jennifer@seismo.berkeley.edu>
2021-02-11 19:21:41 -0800

    Added basic keyboard navigation, increased color contrast for accessibility.

--- scripts/scriptDownload.js
+++ scripts/scriptDownload.js
@@ -31,6 +31,13 @@ function initTableClick () {
       document.location = $(this).data('href');
       return false;
     });
+    // For keyboard navigation
+    $('table tbody tr').keypress(function() {
+      if (event.which == 13){
+        window.location = $(this).data('href');
+      }
+      return false;
+    });
   });
 }
 
@@ -67,8 +74,8 @@ function listProducts (eventid, productList) {
     '<table class="table table-hover table-sm archive_table">' +
     '<thead>' +
     '<tr class="table-dark">' +
-    '<th style="text-align:left" scope="col";>File name</th>' +
-    '<th style="text-align:left" scope="col";>Description</th>' +
+    '<th style="text-align:left;color:black;" scope="col";>File name</th>' +
+    '<th style="text-align:left;color:black;" scope="col";>Description</th>' +
     '</thead>' +
     '</tr>' +
     '<tbody>';
@@ -76,21 +83,21 @@ function listProducts (eventid, productList) {
   var mapType=0;
   for (var k = 0; k < catList.length; k++) {
     if (categories.indexOf(catList[k]) >= 0) {
-      myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#DA6713" style="text-align:left"><font size="3" color="#ffffff"><b>' + catList[k] +
+      myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#BA5711" style="text-align:left"><font size="3" color="#ffffff"><b>' + catList[k] +
        '</b></font></td></tr>';
       for (var i = 0; i < productsNumber; i++) {
         if (productsList[i].cat === catList[k]) {
 
           if (productList[i].name.slice(-5) === '.jpg' && catList[k]==='Peak Ground Motion Maps' && mapType===0) {
-            myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#F6C564" style="text-align:left"><font size="2" color="#ffffff"><b> JPG maps </b></font></td></tr>';
+            myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#BA5711" style="text-align:left"><font size="2" color="#ffffff"><b> JPG maps </b></font></td></tr>';
             mapType = 1;
           }
           if (productList[i].name.slice(-5) === '.pdf' && catList[k]==='Peak Ground Motion Maps' && mapType===1) {
-            myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#F6C564" style="text-align:left"><font size="2" color="#ffffff"><b> PDF maps </b></font></td></tr>';
+            myvar += '<tr data-href="#"><td colspan = "100%" bgcolor="#BA5711" style="text-align:left"><font size="2" color="#ffffff"><b> PDF maps </b></font></td></tr>';
             mapType = 2;
           }
 
-          myvar += '<tr data-href="' + baseLink + productsList[i].name + '">' +
+          myvar += '<tr tabindex="0" data-href="' + baseLink + productsList[i].name + '">' +
               '<td style="text-align:left"><font size="2"> ' +
               productsList[i].name +
               '</font></td>' +
